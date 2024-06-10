e3d1a56bb0acb7138fc788f3605da8c9117ed1ea
mohittanwarCod <officialmohittanwar@gmail.com>
2024-06-10 12:11:11 +0530

    "Added 1d Representation with 18 moves"

--- RubiksCube1dArray.cpp
+++ RubiksCube1dArray.cpp
@@ -1,27 +1,36 @@
+//
+// Created by tanwa on 10-06-2024.
+//
 #include "RubiksCube.h"
 
-class RubiksCube3dArray : public RubiksCube
+class RubiksCube1dArray : public RubiksCube
 {
 private:
-       void rotateFace(int ind)
+    static inline int getIndex(int ind, int row, int col)
+    {
+        return (ind * 9) + (row * 3) + col;
+    }
+
+    void rotateFace(int ind)
     {
         for (int i = 0; i < 3; i++)
         {
             for (int j = i + 1; j < 3; j++)
             {
-                swap(cube[ind][i][j], cube[ind][j][i]);
+                swap(cube[getIndex(ind, i, j)], cube[getIndex(ind, j, i)]);
             }
         }
         for (int i = 0; i < 3; i++)
         {
-            swap(cube[ind][i][0], cube[ind][i][2]);
+
+            swap(cube[getIndex(ind, i, 0)], cube[getIndex(ind, i, 2)]);
         }
     }
 
 public:
-    char cube[6][3][3];
+    char cube[54];
 
-    RubiksCube3dArray()
+    RubiksCube1dArray()
     {
         for (int i = 0; i < 6; i++)
         {
@@ -29,7 +38,7 @@ public:
             {
                 for (int k = 0; j < 3; j++)
                 {
-                    cube[i][j][k] = getColorLetter(COLOR(i));
+                    cube[i * 9 + 3 * j + k] = getColorLetter(COLOR(i));
                 }
             }
         }
@@ -43,7 +52,7 @@ public:
             {
                 for (int k = 0; k < 3; k++)
                 {
-                    if (this->cube[i][j][k] == getColorLetter(COLOR(i)))
+                    if (this->cube[i * 9 + 3 * j + k] == getColorLetter(COLOR(i)))
                         continue;
                     return false;
                 }
@@ -58,22 +67,22 @@ public:
         this->rotateFace(2);
         char temp_arr[3] = {};
         for (int i = 0; i < 3; i++)
-            temp_arr[i] = cube[0][2][i];
+            temp_arr[i] = cube[getIndex(0, 2, i)];
         for (int i = 0; i < 3; i++)
         {
-            cube[0][2][2 - i] = cube[1][i][2];
+            cube[getIndex(0, 2, 2 - i)] = cube[getIndex(1, i, 2)];
         }
         for (int i = 0; i < 3; i++)
         {
-            cube[1][i][2] = cube[5][0][i];
+            cube[getIndex(1, i, 2)] = cube[getIndex(5, 0, i)];
         }
         for (int i = 0; i < 3; i++)
         {
-            cube[5][0][2 - i] = cube[3][i][0];
+            cube[getIndex(5, 0, 2 - i)] = cube[getIndex(3, i, 0)];
         }
         for (int i = 0; i < 3; i++)
         {
-            cube[3][i][0] = temp_arr[i];
+            cube[getIndex(3, i, 0)] = temp_arr[i];
         }
 
         return *this;
@@ -97,17 +106,18 @@ public:
         this->rotateFace(0);
         char arr[3];
         for (int i = 0; i < 3; i++)
-            arr[i] = cube[2][0][i];
+            arr[i] = cube[getIndex(2, 0, i)];
         for (int i = 0; i < 3; i++)
-            cube[2][0][i] = cube[3][0][i];
+            cube[getIndex(2, 0, i)] = cube[getIndex(3, 0, i)];
         for (int i = 0; i < 3; i++)
-            cube[3][0][i] = cube[4][0][i];
+            cube[getIndex(3, 0, i)] = cube[getIndex(4, 0, i)];
         for (int i = 0; i < 3; i++)
-            cube[4][0][i] = cube[1][0][i];
+            cube[getIndex(4, 0, i)] = cube[getIndex(1, 0, i)];
         for (int i = 0; i < 3; i++)
-            cube[1][0][i] = arr[i];
+            cube[getIndex(1, 0, i)] = arr[i];
         return *this;
     }
+
     RubiksCube &u2() override
     {
         this->u();
@@ -128,20 +138,21 @@ public:
         this->rotateFace(1);
         char arr[3];
         for (int i = 0; i < 3; i++)
-            arr[i] = cube[2][i][0];
+
+            arr[i] = cube[getIndex(2, i, 0)];
         for (int i = 0; i < 3; i++)
-            cube[2][0][i] = cube[0][0][i];
+            cube[getIndex(2, i, 0)] = cube[getIndex(0, i, 0)];
 
         // Move left column of the back face to the top face (note the orientation)
         for (int i = 0; i < 3; i++)
-            cube[0][i][0] = cube[4][2 - i][2];
+            cube[getIndex(0, i, 0)] = cube[getIndex(4, 2 - i, 2)];
 
         // Move left column of the bottom face to the back face (note the orientation)
         for (int i = 0; i < 3; i++)
-            cube[4][2 - i][2] = cube[5][i][0];
+            cube[getIndex(4, 2 - i, 2)] = cube[getIndex(5, i, 0)];
         // Move temp array (original left column of the front face) to the bottom face
         for (int i = 0; i < 3; i++)
-            cube[5][i][0] = arr[i];
+            cube[getIndex(5, i, 0)] = arr[i];
         return *this;
     }
 
@@ -166,15 +177,15 @@ public:
         this->rotateFace(4);
         char arr[3];
         for (int i = 0; i < 3; i++)
-            arr[i] = cube[0][0][i];
+            arr[i] = cube[getIndex(0, 0, i)];
         for (int i = 0; i < 3; i++)
-            cube[0][0][i] = cube[3][i][2];
+            cube[getIndex(0, 0, i)] = cube[getIndex(3, i, 2)];
         for (int i = 0; i < 3; i++)
-            cube[3][i][2] = cube[5][2][2 - i];
+            cube[getIndex(3, i, 2)] = cube[getIndex(5, 2, 2 - i)];
         for (int i = 0; i < 3; i++)
-            cube[5][2][i] = cube[1][i][0];
+            cube[getIndex(5, 2, i)] = cube[getIndex(1, i, 0)];
         for (int i = 0; i < 3; i++)
-            cube[1][i][0] = arr[2 - i];
+            cube[getIndex(1, i, 0)] = arr[2 - i];
         return *this;
     }
     RubiksCube &b2() override
@@ -196,15 +207,15 @@ public:
         this->rotateFace(3);
         char arr[3];
         for (int i = 0; i < 3; i++)
-            arr[i] = cube[2][i][2];
+            arr[i] = cube[getIndex(2, i, 2)];
         for (int i = 0; i < 3; i++)
-            cube[2][i][2] = cube[5][i][2];
+            cube[getIndex(2, i, 2)] = cube[getIndex(5, i, 2)];
         for (int i = 0; i < 3; i++)
-            cube[5][i][2] = cube[4][2 - i][0];
+            cube[getIndex(5, i, 2)] = cube[getIndex(4, 2 - i, 0)];
         for (int i = 0; i < 3; i++)
-            cube[4][i][0] = cube[0][2 - i][2];
+            cube[getIndex(4, 2 - i, 0)] = cube[getIndex(0, 2 - i, 2)];
         for (int i = 0; i < 3; i++)
-            cube[0][i][2] = arr[i];
+            cube[getIndex(0, 2 - i, 2)] = arr[i];
         return *this;
     }
     RubiksCube &r2() override
@@ -225,17 +236,17 @@ public:
     {
         this->rotateFace(5);
 
-        char temp_arr[3] = {};
+        char arr[3] = {};
         for (int i = 0; i < 3; i++)
-            temp_arr[i] = cube[2][2][i];
+            arr[i] = cube[getIndex(2, 2, i)];
         for (int i = 0; i < 3; i++)
-            cube[2][2][i] = cube[1][2][i];
+            cube[getIndex(2, 2, i)] = cube[getIndex(1, 2, i)];
         for (int i = 0; i < 3; i++)
-            cube[1][2][i] = cube[4][2][i];
+            cube[getIndex(1, 2, i)] = cube[getIndex(4, 2, i)];
         for (int i = 0; i < 3; i++)
-            cube[4][2][i] = cube[3][2][i];
+            cube[getIndex(4, 2, i)] = cube[getIndex(3, 2, i)];
         for (int i = 0; i < 3; i++)
-            cube[3][2][i] = temp_arr[i];
+            cube[getIndex(3, 2, i)] = arr[i];
 
         return *this;
     }
