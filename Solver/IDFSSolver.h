401b28660c9e7224df56045a5594bc8f9198f0b9
mohittanwarCod <officialmohittanwar@gmail.com>
2024-12-25 22:29:21 +0530

    Update IDFSSolver.h
    Corrected IDDFS Code

--- Solver/IDFSSolver.h
+++ Solver/IDFSSolver.h
@@ -2,58 +2,44 @@
 // Created by tanwa on 15-06-2024.
 //
 #include <bits/stdc++.h>
-using namespace std;
-template <typename T>
 #include "../Model/RubiksCube.h"
-#ifndef RUBIKCUBESOLVER_IDFSSOLVER_H
-#define RUBIKCUBESOLVER_IDFSSOLVER_H
-class IDFSSolver
+#include "DFSSolver.h"
+
+#ifndef RUBIKS_CUBE_SOLVER_IDDFSSOLVER_H
+#define RUBIKS_CUBE_SOLVER_IDDFSSOLVER_H
+
+template <typename T, typename H>
+class IDDFSSolver
 {
+
 private:
+    int max_search_depth;
     vector<RubiksCube::MOVE> moves;
-    T rubikCube;
-    int max_depth;
-
-    bool dfs(int depth)
-    {
-        if (rubikCube.isSolved())
-        {
-            rubikCube.print();
-            return true;
-        }
-
-        if (depth > max_depth)
-            return false;
-        for (int i = 0; i < 18; i++)
-        {
-            moves.push_back(RubiksCube::MOVE(i));
-            rubikCube.move(RubiksCube::MOVE(i));
-            if (dfs(depth + 1))
-                return true;
-            rubikCube.invert(RubiksCube::MOVE(i));
-            moves.pop_back();
-        }
-        return false;
-    }
 
 public:
-    int max_depth_val;
-    IDFSSolver(T rubikCube, int max_depth_val = 8)
+    T rubiksCube;
+
+    IDDFSSolver(T _rubiksCube, int _max_search_depth = 7)
     {
-        this->rubikCube = rubikCube;
-        this->max_depth_val = max_depth_val;
+        rubiksCube = _rubiksCube;
+        max_search_depth = _max_search_depth;
     }
+
+    //    Used DFSSolver with increasing max_search_depth
     vector<RubiksCube::MOVE> solve()
     {
-        for (int i = 1; i <= max_depth_val; i++)
+        for (int i = 1; i <= max_search_depth; i++)
         {
-            max_depth = i;
-            if (dfs(1))
-                return moves;
+            DFSSolver<T, H> dfsSolver(rubiksCube, i);
+            moves = dfsSolver.solve();
+            if (dfsSolver.rubiksCube.isSolved())
+            {
+                rubiksCube = dfsSolver.rubiksCube;
+                break;
+            }
         }
-
         return moves;
     }
 };
 
-#endif // RUBIKCUBESOLVER_IDFSSOLVER_H
+#endif // RUBIKS_CUBE_SOLVER_IDDFSSOLVER_H
